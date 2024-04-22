55ff9592daeee394e517d36ab66d407e62f46ee1
Myyk Seok <2080820+myyk@users.noreply.github.com>
2024-04-22 16:34:33 +0800

    feat: remove update functionality

--- addons/playlists/playlists.gd
+++ addons/playlists/playlists.gd
@@ -2,21 +2,20 @@
 extends EditorPlugin
 
 func _enter_tree():
-	Engine.set_meta("PlaylistsEditorPlugin", self)
-	
 	# Initialization of the plugin goes here.
 	add_custom_type("AudioStreamDirectoryPlayer", "AudioStreamPlayer", preload("audio/audio_stream_directory_player.gd"), preload("images/AudioStream.svg"))
 	add_custom_type("AudioStreamPlaylistPlayer", "AudioStreamPlayer", preload("res://addons/playlists/audio/audio_stream_playlist_player.gd"), preload("images/AudioStream.svg"))
 	
 	# Add auto-update functionality
-	if Engine.is_editor_hint():
-		var update_tool: Node = load("res://addons/playlists/updater2/download_update_panel.tscn").instantiate()
-		Engine.get_main_loop().root.call_deferred("add_child", update_tool)
+	#if Engine.is_editor_hint():
+		#Engine.set_meta("PlaylistsEditorPlugin", self)
+		#var update_tool: Node = load("res://addons/playlists/updater2/download_update_panel.tscn").instantiate()
+		#Engine.get_main_loop().root.call_deferred("add_child", update_tool)
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
 	remove_custom_type("AudioStreamDirectoryPlayer")
 	remove_custom_type("AudioStreamPlaylistPlayer")
 
-	if Engine.has_meta("PlaylistsEditorPlugin"):
-		Engine.remove_meta("PlaylistsEditorPlugin")
+	#if Engine.has_meta("PlaylistsEditorPlugin"):
+		#Engine.remove_meta("PlaylistsEditorPlugin")
