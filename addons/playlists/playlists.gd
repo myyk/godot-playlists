e74c815df37859c0e512930327fc95814b2b7f29
Myyk Seok <2080820+myyk@users.noreply.github.com>
2024-04-23 20:43:07 +0800

    feat: remove custom_types because it already has a class_name to prevent future errors

--- addons/playlists/playlists.gd
+++ addons/playlists/playlists.gd
@@ -3,8 +3,6 @@ extends EditorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_custom_type("AudioStreamDirectoryPlayer", "AudioStreamPlayer", preload("audio/audio_stream_directory_player.gd"), preload("images/AudioStream.svg"))
-	add_custom_type("AudioStreamPlaylistPlayer", "AudioStreamPlayer", preload("res://addons/playlists/audio/audio_stream_playlist_player.gd"), preload("images/AudioStream.svg"))
 	
 	# Add auto-update functionality
 	if Engine.is_editor_hint():
@@ -14,8 +12,6 @@ func _enter_tree():
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
-	remove_custom_type("AudioStreamDirectoryPlayer")
-	remove_custom_type("AudioStreamPlaylistPlayer")
-
+	
 	if Engine.has_meta("PlaylistsEditorPlugin"):
 		Engine.remove_meta("PlaylistsEditorPlugin")
